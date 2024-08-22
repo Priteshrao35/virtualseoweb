efeb31494d882f3d7419b3d03218ee834852743d
Priteshrao35 <devepritesh@gmail.com>
2024-08-22 23:02:57 +0530

    services page updated

--- app/homepage/servicesdetails/[servicesid]/page.jsx
+++ app/homepage/servicesdetails/[servicesid]/page.jsx
@@ -1,25 +1,35 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Navbar from '../../navbar';
-import Header from '../../header';
-import OurTeam from '../../ourteam';
-import FooterSection from '../../footer';
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../../navbar";
+import Header from "../../header";
+import FooterSection from "../../footer";
+import { Image } from "antd";
 
-// Function to create a slug from a name
 const createSlug = (name) => {
-  return name.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+  return name
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
 };
 
 const ServiceDetailsPage = ({ params }) => {
   const { servicesid } = params;
   const [service, setService] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    mobile: "",
+    message: ""
+  });
+  const [formStatus, setFormStatus] = useState("");
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     axios
-      .get('https://virtualseoweb.pythonanywhere.com/menu-items/')
+      .get("https://virtualseoweb.pythonanywhere.com/menu-items/")
       .then((response) => {
         const data = response.data;
         const service = data.find(
@@ -31,84 +41,187 @@ const ServiceDetailsPage = ({ params }) => {
         setLoading(false);
       })
       .catch((error) => {
-        console.error('Error fetching service details:', error);
+        console.error("Error fetching service details:", error);
         setLoading(false);
       });
   }, [servicesid]);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.name.trim()) newErrors.name = "Name is required";
+    if (!formData.email.trim()) newErrors.email = "Email is required";
+    else if (!/\S+@\S+\.\S+/.test(formData.email))
+      newErrors.email = "Email is invalid";
+    if (!formData.subject.trim()) newErrors.subject = "Subject is required";
+    if (!formData.mobile.trim()) newErrors.mobile = "Mobile number is required";
+    else if (!/^\d{10}$/.test(formData.mobile))
+      newErrors.mobile = "Mobile number must be 10 digits";
+    if (!formData.message.trim()) newErrors.message = "Message is required";
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    try {
+      const response = await axios.post(
+        "https://virtualseoweb.pythonanywhere.com/send-email",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      if (response.status === 200) {
+        setFormStatus(
+          "Email sent successfully! A confirmation message has been sent to your email."
+        );
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          mobile: "",
+          message: ""
+        });
+        setErrors({});
+      }
+    } catch (error) {
+      setFormStatus("Failed to send email.");
+      console.error("Error sending form details:", error);
+    }
+  };
+
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="text-black">Loading...</div>;
   }
 
   if (!service) {
-    return <div>Service not found</div>;
+    return <div className="text-black">Service not found</div>;
   }
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 text-black">
       <Header />
       <hr />
       <Navbar />
 
-      <div className="container mx-auto p-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-40">
+      <Image
+        src={service.Service_Image}
+        alt={service.name}
+        className="w-full h-auto object-cover"
+        preview={false}
+      />
+
+      <div className="px-[12em] mt-20">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-40">
           {/* Content Section */}
-          <div className="p-6 rounded-lg max-w-full">
-            <h2 className="text-2xl font-semibold mb-4 text-black">
-              {service.name}
-            </h2>
-            <p className="text-black">{service.Content}</p>
+          <div className="p-6 rounded-lg max-w-full col-span-2">
+            <h2 className="text-2xl font-semibold mb-4">{service.name}</h2>
+            <p>{service.Content}</p>
           </div>
 
           {/* Form Section */}
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-[25em] transform transition-transform hover:scale-105">
-            <h2 className="text-xl font-semibold mb-4 text-black">
+          <div className="bg-white p-4 rounded-lg shadow-lg transform transition-transform hover:scale-105 col-span-1">
+            <h2 className="text-xl font-semibold mb-3">
               Get a Free Consultation
             </h2>
-            <form action="/submit" method="post">
-              <div className="mb-4">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Name:
-                </label>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-5">
                 <input
                   type="text"
                   id="name"
                   name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
                   required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Name"
+                  className={`mt-1 block w-full px-2 py-2 border ${
+                    errors.name ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
                 />
+                {errors.name && (
+                  <p className="text-red-600 text-sm">{errors.name}</p>
+                )}
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email:
-                </label>
+              <div className="mb-5">
                 <input
                   type="email"
                   id="email"
                   name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
                   required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Email"
+                  className={`mt-1 block w-full px-2 py-2 border ${
+                    errors.email ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
                 />
+                {errors.email && (
+                  <p className="text-red-600 text-sm">{errors.email}</p>
+                )}
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Message:
-                </label>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Subject"
+                  className={`mt-1 block w-full px-2 py-1 border ${
+                    errors.subject ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                />
+                {errors.subject && (
+                  <p className="text-red-600 text-sm">{errors.subject}</p>
+                )}
+              </div>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  id="mobile"
+                  name="mobile"
+                  value={formData.mobile}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Mobile"
+                  className={`mt-1 block w-full px-2 py-2 border ${
+                    errors.mobile ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                />
+                {errors.mobile && (
+                  <p className="text-red-600 text-sm">{errors.mobile}</p>
+                )}
+              </div>
+              <div className="mb-5">
                 <textarea
                   id="message"
                   name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
                   required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Message"
+                  className={`mt-1 block w-full px-2 py-2 border ${
+                    errors.message ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                  style={{ minHeight: "80px" }}
                 />
+                {errors.message && (
+                  <p className="text-red-600 text-sm">{errors.message}</p>
+                )}
               </div>
               <button
                 type="submit"
@@ -117,6 +230,17 @@ const ServiceDetailsPage = ({ params }) => {
                 Send
               </button>
             </form>
+            {formStatus && (
+              <p
+                className={`mt-4 text-sm ${
+                  formStatus.includes("successfully")
+                    ? "text-green-600"
+                    : "text-red-600"
+                }`}
+              >
+                {formStatus}
+              </p>
+            )}
           </div>
         </div>
       </div>
